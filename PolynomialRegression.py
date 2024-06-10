ad62c89f4016dccdf7152a3a61a4d14680b9ad05
akashrajput1x <akash7f@gmail.com>
2024-06-10 14:56:46 +0530

    commented and tested in multiple degrees of polynomial

--- PolynomialRegression.py
+++ PolynomialRegression.py
@@ -1,13 +1,29 @@
 import numpy as np
 
-class Regression:
+class PolynomialRegression:
     
-    def fit(self, x, y, learning_rate = 0.001, no_of_iterations = 100000):
+    def fit(self, x, y, learning_rate = 1, no_of_iterations = 10000):
+        """
+        Use standardized lying between 0 and 1
+        m samples of one dimensional data
+        x = [x1, x2, x3...xm]
 
-        if not isinstance(x[0], np.ndarray):
+        m samples of n dimensional data
+        x = [[x11, x12...x1n],
+             [x21, x22...[2n],
+             ...
+             [xm1, xm2...[mn]]
+    
+        m outputs for m samples of data
+        y = [y1, y2, y3...ym]
+
+        """
+        if not isinstance(type(x[0]), np.ndarray):
             x = x.reshape(-1, 1)
+
+        # no of attributes in data and no of samples of data
         attributes = len(x[0])
-        samples = len(y)
+        samples = len(x)
         
         a = learning_rate                               #learning rate
         n = no_of_iterations                            #no of iterations
@@ -16,7 +32,6 @@ class Regression:
         self.b = 0                                      #constant
 
         for i in range(0, n):
-            
 
             # predicting values using row approach
             # yh = np.zeros(samples)
@@ -24,13 +39,17 @@ class Regression:
             #     yh[i] = self.predict(x[i])
 
             # predicting values column approach
+            # column is preferred becuase of parallel multiplication of vector
             yh = np.zeros(samples)
             for i in range(attributes):
                 attribute = x[:, i]
                 yh += self.w[i] * np.power(attribute, self.p[i])
             yh += self.b
             
+            # error between predicted value and true value
             error = yh - y
+            
+            # reducing of error using gradient descent
             # gradient descent with respect to power of each attribute
             for i in range(attributes):
                 attribute = x[:, i]
@@ -46,6 +65,7 @@ class Regression:
             # gradient descent with respect to constant
             self.b -= a * error.mean()
 
+        # mean square error on training data
         self.MSE = (error**2).mean()
 
     def predict(self, x):
@@ -60,11 +80,10 @@ class Regression:
         print()
 
 if __name__ == "__main__":
-    import time
 
-    x = np.array([1, 2, 3, 4, 5])     #input values
-    y = np.array([1, 4, 9, 16, 25])                                   #output values
-    model = Regression()
+    x = np.array([0.1, 0.3, 0.5, 0.7, 1])
+    y = np.array([0.001, 0.027, 0.125, 0.343, 1])
+    model = PolynomialRegression()
     model.fit(x, y)
     model.print()
 
